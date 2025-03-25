6ddcd3d8fbb13da24141e96d5ea828cf106170ce
lorenekeda <lorenekeda@gmail.com>
2025-03-24 21:37:48 -0400

    updated servlet dependency plus addition of customer update servlet with required imports

--- src/main/java/com/UpdateCustomerServlet.java
+++ src/main/java/com/UpdateCustomerServlet.java
@@ -1,10 +1,14 @@
 package com;
-import jakarta.servlet.ServletException;
+import java.io.IOException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.ServletException;
 
-public class UpdateCustomerServlet extends HttpServlet {
+
+
+
+
+public class UpdateCustomerServlet  {
 }
