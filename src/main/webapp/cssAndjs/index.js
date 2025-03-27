b6b0a897485f60bf31ca43ff57dd0c36578abbf8
sophia <sophiamiemie@gmail.com>
2025-03-27 14:57:17 -0400

    added a comment

--- src/main/webapp/cssAndjs/index.js
+++ src/main/webapp/cssAndjs/index.js
@@ -1,5 +1,6 @@
 const signinButton = document.getElementById("signinbutton")
 signinButton.addEventListener("click", function(){
+//check if the user exists
  windows.location.href = "welcomeuser.jsp";
 
 })
