ba2ae5e7076001ba502f09a0cb646ea0d8090a48
mahita <mahita1818@gmail.com>
2025-03-29 14:35:02 -0400

    get customer booking query

--- src/main/java/com/BookingService.java
+++ src/main/java/com/BookingService.java
@@ -101,6 +101,53 @@ public class BookingService {
         }
     }
 
+    /**
+     * Method to get all Booking that belong to a Customer
+     *
+     * @return List of com.Booking from database
+     * @throws Exception when trying to connect to database
+     */
+    public List<Booking> getCustomerBooking(String customerId) throws Exception {
+
+        String sql = "SELECT * FROM relational_schema.booking WHERE customer_id = ?";
+
+        ConnectionDB db = new ConnectionDB();
+
+        List<Booking> bookings = new ArrayList<>();
+
+        //connect to database, catch any exceptions
+        try (Connection con = db.getConnection()) {
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, customerId);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                // create com.Booking object from result
+                Booking booking = new Booking(
+                        rs.getInt("chain_id"),
+                        rs.getInt("hotel_id"),
+                        rs.getInt("room_num"),
+                        rs.getString("booking_start_date"),
+                        rs.getString("booking_end_date"),
+                        rs.getString("customer_id")
+                );
+
+                bookings.add(booking);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+            db.close();
+
+            return bookings;
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
     public static void createBooking(Date start, Date end, Integer chainId, Integer hotelId, Integer roomNum, String customerId) throws Exception {
         String sql = "INSERT INTO relational_schema.booking (booking_start_date, booking_end_date, room_num, customer_id, hotel_id, chain_id) VALUES (?, ?, ?, ?, ?, ?)";
 
