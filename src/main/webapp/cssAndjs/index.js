e4eb9a9d2283d563f6a1f9363a85bfbfc9dd0b91
sophia <sophiamiemie@gmail.com>
2025-03-29 12:52:36 -0400

    added comment

--- src/main/webapp/cssAndjs/index.js
+++ src/main/webapp/cssAndjs/index.js
@@ -4,5 +4,6 @@ signinButton.addEventListener("click", function(){
  windows.location.href = "welcomeuser.jsp";
 
 })
+//use localstorage with customer ids and then try to match it with one in the database
 
 
